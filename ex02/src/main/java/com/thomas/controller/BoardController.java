2744dcc580bd3f881ecb03dc6806f88f8b129d41
hdkim <hdkim.scala@gmail.com>
2020-03-15 20:12:26 +0900

    chapter 10 - finish

---
+++ ex02/src/main/java/com/thomas/controller/BoardController.java
@@ -0,0 +1,143 @@
+package com.thomas.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.thomas.service.BoardService;
+import com.thomas.domain.BoardVO;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@Log4j
+@RequestMapping("/board/*")
+@AllArgsConstructor
+public class BoardController {
+
+	private BoardService service;
+
+	@GetMapping("/register")
+	public void register() {
+
+	}
+
+	@GetMapping("/list")
+	public void list(Model model) {
+		log.info("list");
+		model.addAttribute("list", service.getList());
+	}
+
+	// @GetMapping("/list")
+	// public void list(Criteria cri, Model model) {
+	//
+	// log.info("list: " + cri);
+	// model.addAttribute("list", service.getList(cri));
+	//
+	// }
+
+/*	@GetMapping("/list")
+	public void list(Criteria cri, Model model) {
+
+		log.info("list: " + cri);
+		model.addAttribute("list", service.getList(cri));
+		// model.addAttribute("pageMaker", new PageDTO(cri, 123));
+
+		int total = service.getTotal(cri);
+
+		log.info("total: " + total);
+
+		model.addAttribute("pageMaker", new PageDTO(cri, total));
+
+	}*/
+
+	/*
+		(1) 등록 작업이 끝난 후 다시 목록 화면으로 이동
+		(2) 리다이렉트 되어도, 새로 등록된 게시물의 번호를 같이 전달하기 위해서 RedirectAttributes 를 이용
+		(3) 리턴할 때 'redirect:' 사용     <- 내부적으로 response.sendRedirect() 처리
+	 */
+	@PostMapping("/register")
+	public String register(BoardVO board, RedirectAttributes rttr) {
+		log.info("register: " + board);
+		service.register(board);
+		rttr.addFlashAttribute("result", board.getBno());
+
+		return "redirect:/board/list";
+	}
+
+	@GetMapping({ "/get", "/modify" })
+	public void get(@RequestParam("bno") Long bno, Model model) {
+		log.info("/get or modify");
+
+		// 화면 쪽으로 해당 번호의 게실물을 전달해야 하므로
+		model.addAttribute("board", service.get(bno));
+	}
+
+/*	@GetMapping({ "/get", "/modify" })
+	public void get(@RequestParam("bno") Long bno, @ModelAttribute("cri") Criteria cri, Model model) {
+
+		log.info("/get or modify");
+		model.addAttribute("board", service.get(bno));
+	}*/
+
+	@PostMapping("/modify")
+	public String modify(BoardVO board, RedirectAttributes rttr) {
+		log.info("modify:" + board);
+
+		if (service.modify(board)) {
+			rttr.addFlashAttribute("result", "success");
+		}
+
+		return "redirect:/board/list";
+	}
+
+/*	@PostMapping("/modify")
+	public String modify(BoardVO board, @ModelAttribute("cri") Criteria cri, RedirectAttributes rttr) {
+		log.info("modify:" + board);
+
+		if (service.modify(board)) {
+			rttr.addFlashAttribute("result", "success");
+		}
+
+		rttr.addAttribute("pageNum", cri.getPageNum());
+		rttr.addAttribute("amount", cri.getAmount());
+		rttr.addAttribute("type", cri.getType());
+		rttr.addAttribute("keyword", cri.getKeyword());
+
+		return "redirect:/board/list";
+	}*/
+
+	// 삭제 후 페이지 이동	<- RedirectAttributes 사용
+	@PostMapping("/remove")
+	public String remove(@RequestParam("bno") Long bno, RedirectAttributes rttr) {
+		log.info("remove..." + bno);
+
+		if (service.remove(bno)) {
+			rttr.addFlashAttribute("result", "success");
+		}
+
+		return "redirect:/board/list";
+	}
+
+/*	@PostMapping("/remove")
+	public String remove(@RequestParam("bno") Long bno, Criteria cri, RedirectAttributes rttr) {
+
+		log.info("remove..." + bno);
+		if (service.remove(bno)) {
+			rttr.addFlashAttribute("result", "success");
+		}
+		rttr.addAttribute("pageNum", cri.getPageNum());
+		rttr.addAttribute("amount", cri.getAmount());
+		rttr.addAttribute("type", cri.getType());
+		rttr.addAttribute("keyword", cri.getKeyword());
+
+		return "redirect:/board/list";
+	}*/
+
+}
