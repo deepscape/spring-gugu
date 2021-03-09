40a54b42e2ed0446e15a67939cda2cdc42ec0ff5
deepscape <hdkim.scala@gmail.com>
2021-03-09 17:48:37 +0900

    reply test code

--- ex03/src/main/java/com/thomas/mapper/ReplyMapper.java
+++ ex03/src/main/java/com/thomas/mapper/ReplyMapper.java
@@ -1,5 +1,18 @@
 package com.thomas.mapper;
 
+import com.thomas.domain.Criteria;
+import com.thomas.domain.ReplyVO;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
 public interface ReplyMapper {
 
+    public int insert(ReplyVO vo);
+    public ReplyVO read(Long bno);
+    public int delete (Long rno);
+    public int update (ReplyVO reply);
+
+    // 2개 이상의 데이터를 파라미터로 전달하려면 1) 별도의 객체 2) Map 3) @Param
+    public List<ReplyVO> getListWithPaging(@Param("cri") Criteria cri, @Param("bno") Long bno);
 }
