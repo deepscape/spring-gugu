08ca7156a1285ec8a880c75dfb1f39a4799deaf9
deepscape <hdkim.scala@gmail.com>
2021-03-10 15:50:51 +0900

    controller finish

---
+++ ex03/src/main/java/com/thomas/service/ReplyService.java
@@ -0,0 +1,15 @@
+package com.thomas.service;
+
+import com.thomas.domain.Criteria;
+import com.thomas.domain.ReplyVO;
+
+import java.util.List;
+
+public interface ReplyService {
+
+    public int register(ReplyVO vo);
+    public ReplyVO get(Long rno);
+    public int modify(ReplyVO vo);
+    public int remove(Long rno);
+    public List<ReplyVO> getList(Criteria cri, Long bno);
+}
