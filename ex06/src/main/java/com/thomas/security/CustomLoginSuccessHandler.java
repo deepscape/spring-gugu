ba52ee8c54509c2678d420213eba72e5518704b1
deepscape <hdkim.scala@gmail.com>
2021-03-28 15:51:47 +0900

    chapter 37. finish

--- ex06/src/main/java/com/thomas/security/CustomLoginSuccessHandler.java
+++ ex06/src/main/java/com/thomas/security/CustomLoginSuccessHandler.java
@@ -2,7 +2,6 @@ package com.thomas.security;
 
 import lombok.extern.log4j.Log4j;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 import javax.servlet.ServletException;
